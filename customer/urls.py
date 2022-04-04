b3bf136bfa9dc87574a3fa5e1a7c76d5b6829949
shiblykt <shiblykt56@gmail.com>
2022-04-04 23:49:42 +0530

    initial commit

---
+++ customer/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('login',views.login,name='login'),
+    path('signup',views.signup,name='signup'),
+    path('forgotpassword',views.forgotpassword,name='forgotpassword'),
+    path('changepassword',views.changepassword,name='changepassword'),
+    path('profile',views.customerprofile,name='customerprofile'),
+    path('store',views.store,name='store'),
+
+]
---
+++ customer/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return render(request,'myapp/index.html')
+
+def login(request):
+    return render(request,'myapp/cust_login.html')
+
+def signup(request):
+    return render(request,'myapp/cust_signup.html')
+
+def forgotpassword(request):
+    return render(request,'myapp/forgotpassword.html')
+
+def changepassword(request):
+    return render(request,'myapp/cust_changepass.html')
+
+def customerprofile(request):
+    return render(request,'myapp/cust_profile.html')
+
+def store(request):
+    return render(request,'myapp/store.html')
