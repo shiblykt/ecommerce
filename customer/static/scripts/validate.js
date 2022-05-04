c78d5203f7a96bc87fcb28ec71097a011db550a0
shiblykt <shiblykt56@gmail.com>
2022-05-04 14:58:00 +0530

    signup validation updated

--- customer/static/scripts/validate.js
+++ customer/static/scripts/validate.js
@@ -24,6 +24,92 @@ function login_validation(){
 
 function signup_validation(){
     st=1
-    document.getElementById("user_typ")
+    usertype = document.getElementById("usertyp")
+    if(usertyp.value=="reseller"){
+        var a=1
+    }
+    else{
+        // validate firstname
+        fname = document.getElementById("fname")
+        if(fname.value==""){
+            fname.style.border = "1px red solid"
+            st = 0
+        }else{
+            fname.style.border = "1px green solid"
+        }
+
+        // validate dob
+        dob = document.getElementById("dob")
+        if(dob.value==""){
+            dob.style.border = "1px red solid"
+            st = 0
+        }else{
+            dob.style.border = "1px green solid"
+        }
+
+        // validate address
+        adrs = document.getElementById("adrs")
+        if(adrs.value==""){
+            adrs.style.border = "1px red solid"
+            st = 0
+        }else{
+            adrs.style.border = "1px green solid"
+        }
+
+        // validate country
+        country = document.getElementById("cntry")
+        if(cntry.value==""){
+            cntry.style.border = "1px red solid"
+            st = 0
+        }else{
+            cntry.style.border = "1px green solid"
+        }
+
+        // validate mobile number
+        var mre = /^\d{10}$/
+        mobile = document.getElementById("userMobile")
+        if(mobile.value.match(mre)){
+            mobile.style.border = "1px green solid"
+        }else{
+            mobile.style.border = "1px red solid"
+            st = 0
+        }
+
+        // validate email
+        var ere = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+        email = document.getElementById("userEmail")
+        if(email.value.match(ere)){
+            email.style.border = "1px green solid"
+        }else{
+            email.style.border = "1px red solid"
+            st = 0
+        }
+
+        // validate whether password input is empty
+        password = document.getElementById("userPassword")
+        password2 = document.getElementById("passwordCnf")
+        if(password.value==""){
+            password.style.border = "1px red solid"
+            st = 0
+        }else{
+            password.style.border = "1px green solid"
+        }
+
+        // validate whether password and cnfirm passwd are equal
+        
+        if(password.value!=password2.value){
+            password2.style.border = "1px red solid"
+            document.getElementById("pswerror").innerHTML = "passwords are not matching"
+            st = 0
+        }else{
+            password2.style.border = "1px red solid"
+             
+        }
+
+
+        if(st==0){
+            return false
+        }
+    }
 }
 
